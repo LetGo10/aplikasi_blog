5738221873eb78e9df7525b58bd3127175897e07
syazwani <syazhira10@gmail.com>
2025-08-07 23:50:46 +0800

    tugasan ke 3.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,6 +4,8 @@
     <nav class="space-x-4">
         <a href="{{ route('main') }}" class="text-white hover:underline {{ request()->routeIs('main') ? 'font-bold underline' : '' }}">Laman Utama</a>
         <a href="{{ route('about') }}" class="text-white hover:underline {{ request()->routeIs('about') ? 'font-bold underline' : '' }}">Tentang Kami</a>
+        <a href="{{ route('posts') }}" class="text-white hover:underline {{ request()->routeIs('posts') ? 'font-bold underline' : '' }}">Destinasi</a>
+        <a href="{{ route('contact') }}" class="text-white hover:underline {{ request()->routeIs('contact') ? 'font-bold underline' : '' }}">Hubungi Kami</a>
         <a href="#" class="bg-white text-green-700 px-4 py-2 rounded-md text-sm font-medium hover:bg-green-100 transition">Login</a>
     </nav>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="ms">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Air Terjun di Selangor</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-  </head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+        <!-- Styles / Scripts -->
+        <x-style />
+    </head>
+
   <body class="bg-white text-gray-900">
 
     <!-- Header -->
